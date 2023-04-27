42c1fcb76a0033ee2dac6f24974aa1ef701f0f2d
cesar <cesare.munoz@gmail.com>
2023-04-27 01:12:10 -0500

    Se integró la clase mensaje

--- src/main/java/HolaMundo.java
+++ src/main/java/HolaMundo.java
@@ -1,5 +1,7 @@
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!!");
+        Mensaje msg = new Mensaje();
+        String nombre = msg.solicitarDatos("Escribe tu nombre: ");
+        msg.saludar(nombre);
     }
 }
