0489670c8660d913e5f1e1ccb8129cb84c603974
cesar <cesare.munoz@gmail.com>
2023-08-18 11:11:52 -0500

    Se quitó la línea de código inservible

--- src/main/java/com/acme/PersonaRegistro.java
+++ src/main/java/com/acme/PersonaRegistro.java
@@ -4,8 +4,6 @@ public record PersonaRegistro(String nombre, String apellidos) {
 
     @Override
     public String toString() {
-        //César, comenté la linea de codigo que considero que se puede eliminar
-        //System.out.println("Esta instrucción es inservible");
         return "%s %s".formatted(nombre, apellidos);
     }
 }
